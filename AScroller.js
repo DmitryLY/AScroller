7f60488b2622a5bb84e8ea05241806cf049b23b2
Dmitry Y. Lepikhin <79738532+DmitryLY@users.noreply.github.com>
2022-03-04 01:22:56 +0500

    Add files via upload

--- AScroller.js
+++ AScroller.js
@@ -58,6 +58,8 @@ function AScroller(scroller_in, curElement){
 
   if(data_ascroller){
 
+    scroller_in.setAttribute("data-ascroller", "");
+
     var gallery = data_ascroller.search(/(^|\s+)gallery\b/) !== -1 ? true : false;
 
     if(gallery)
@@ -251,8 +253,13 @@ function AScroller(scroller_in, curElement){
         scroller.appendChild(scroller_in.arrow_prev);
         scroller.appendChild(scroller_in.arrow_next);
 
-        scroller_in.arrow_next.innerHTML = '    <svg width="19" height="16" viewBox="0 0 19 16">    <path d="M18.7071 8.70711C19.0976 8.31658 19.0976 7.68342 18.7071 7.29289L12.3431 0.928932C11.9526 0.538408 11.3195 0.538408 10.9289 0.928932C10.5384 1.31946 10.5384 1.95262 10.9289 2.34315L16.5858 8L10.9289 13.6569C10.5384 14.0474 10.5384 14.6805 10.9289 15.0711C11.3195 15.4616 11.9526 15.4616 12.3431 15.0711L18.7071 8.70711ZM0 9H18V7H0V9Z" />    </svg>    ';
-        scroller_in.arrow_prev.innerHTML = '    <svg width="19" height="16" viewBox="0 0 19 16"">    <path transform="rotate(-180 9.499987602233887,8.000007629394531) " d="m18.7071,8.70711c0.3905,-0.39053 0.3905,-1.02369 0,-1.41422l-6.364,-6.36396c-0.3905,-0.39052 -1.0236,-0.39052 -1.4142,0c-0.3905,0.39053 -0.3905,1.02369 0,1.41422l5.6569,5.65685l-5.6569,5.6569c-0.3905,0.3905 -0.3905,1.0236 0,1.4142c0.3906,0.3905 1.0237,0.3905 1.4142,0l6.364,-6.36399zm-18.7071,0.29289l18,0l0,-2l-18,0l0,2z" />    </svg>    ';
+        if( scroller_in.mini || !scroller_in.parent ){
+          scroller_in.arrow_next.innerHTML = '    <svg width="19" height="16" viewBox="0 0 19 16">    <path d="M18.7071 8.70711C19.0976 8.31658 19.0976 7.68342 18.7071 7.29289L12.3431 0.928932C11.9526 0.538408 11.3195 0.538408 10.9289 0.928932C10.5384 1.31946 10.5384 1.95262 10.9289 2.34315L16.5858 8L10.9289 13.6569C10.5384 14.0474 10.5384 14.6805 10.9289 15.0711C11.3195 15.4616 11.9526 15.4616 12.3431 15.0711L18.7071 8.70711ZM0 9H18V7H0V9Z" />    </svg>    ';
+          scroller_in.arrow_prev.innerHTML = '    <svg width="19" height="16" viewBox="0 0 19 16"">    <path transform="rotate(-180 9.499987602233887,8.000007629394531) " d="m18.7071,8.70711c0.3905,-0.39053 0.3905,-1.02369 0,-1.41422l-6.364,-6.36396c-0.3905,-0.39052 -1.0236,-0.39052 -1.4142,0c-0.3905,0.39053 -0.3905,1.02369 0,1.41422l5.6569,5.65685l-5.6569,5.6569c-0.3905,0.3905 -0.3905,1.0236 0,1.4142c0.3906,0.3905 1.0237,0.3905 1.4142,0l6.364,-6.36399zm-18.7071,0.29289l18,0l0,-2l-18,0l0,2z" />    </svg>    ';
+        }else if( scroller_in.parent ){
+          scroller_in.arrow_next.innerHTML = '    <svg width="19" height="16" viewBox="4 0 19 16">    <path d="M 18.7071 8.7071 C 19.0976 8.3166 19.0976 7.6834 18.7071 7.2929 L 12.3431 0.9289 C 11.9526 0.5384 11.3195 0.5384 10.9289 0.9289 C 10.5384 1.3195 10.5384 1.9526 10.9289 2.3432 L 16.5858 8 L 10.9289 13.6569 C 10.5384 14.0474 10.5384 14.6805 10.9289 15.0711 C 11.3195 15.4616 11.9526 15.4616 12.3431 15.0711 L 18.7071 8.7071 Z Z" />    </svg>    ';
+          scroller_in.arrow_prev.innerHTML = '    <svg width="19" height="16" viewBox="-4 0 19 16"">    <path transform="rotate(-180 9.499987602233887,8.000007629394531) " d="M 18.7071 8.7071 C 19.0976 8.3166 19.0976 7.6834 18.7071 7.2929 L 12.3431 0.9289 C 11.9526 0.5384 11.3195 0.5384 10.9289 0.9289 C 10.5384 1.3195 10.5384 1.9526 10.9289 2.3432 L 16.5858 8 L 10.9289 13.6569 C 10.5384 14.0474 10.5384 14.6805 10.9289 15.0711 C 11.3195 15.4616 11.9526 15.4616 12.3431 15.0711 L 18.7071 8.7071 Z Z" />    </svg>    ';
+        }
 
         scroller_in.arrow_prev.ondragstart = scroller_in.arrow_prev.ondrop = scroller_in.arrow_prev.onselectstart = function(){return false;};
         scroller_in.arrow_next.ondragstart = scroller_in.arrow_next.ondrop = scroller_in.arrow_next.onselectstart = function(){return false;};
@@ -271,59 +278,47 @@ function AScroller(scroller_in, curElement){
             var height = 0;
             var max_w_el , max_h_el, max_m_b = 0, max_m_t = 0, max_m_l = 0, max_m_r = 0;
 
-            /// максимальный по размеру элемент
-          for(var i = 0; i < scroll_i.children.length; i++){
-            sc_i = scroll_i.children[i]; 
+          
+            for(var i = 0; i < scroll_i.children.length; i++){
+              sc_i = scroll_i.children[i]; 
 
               var m_l = parseInt( window.getComputedStyle(sc_i).marginLeft );
-            if( m_l > max_m_l ) max_m_l = m_l;
               var m_r = parseInt( window.getComputedStyle(sc_i).marginRight );
-            if( m_r > max_m_r ) max_m_r = m_r;
-              
-  
-              var width_i = sc_i.offsetWidth;
-              if( width_i > width ){
-                width = width_i ;
-                max_w_el = sc_i;
-              } 
+    
+                var width_i = sc_i.offsetWidth + m_l + m_r;
+                if( width_i > width ){
+                  width = width_i ;
+                  max_w_el = sc_i;
+                } 
 
-              var m_t = parseInt( window.getComputedStyle(sc_i).marginBottom );
-            if( m_t > max_m_t ) max_m_t = m_t;
-              var m_b = parseInt( window.getComputedStyle(sc_i).marginTop );
-            if( m_b > max_m_b ) max_m_b = m_b;
-              var height_i = sc_i.offsetHeight;
-            if( height_i > height ){
-                height = height_i ;
-                max_h_el = sc_i;
-              }
-            
-          }
+                var m_t = parseInt( window.getComputedStyle(sc_i).marginBottom );
+                var m_b = parseInt( window.getComputedStyle(sc_i).marginTop );
+
+                var height_i = sc_i.offsetHeight + m_t + m_b;
+              if( height_i > height ){
+                  height = height_i ;
+                  max_h_el = sc_i;
+                }
+
+                sc_i.dim = {m_l: m_l, m_r: m_r, m_t: m_t, m_b: m_b};
+              
+            }
+
+          if( nativeStyleWidth && nativeStyleWidth < width )
+            width = nativeStyleWidth;
 
         
         var parentWidth = parent.offsetWidth - parseInt(window.getComputedStyle(parent).paddingLeft) - parseInt(window.getComputedStyle(parent).paddingRight);
 
-
-        
-        if( nativeStyleWidth && nativeStyleWidth < width )
-          width = nativeStyleWidth;
-          
 
         if( nativeStyleHeight && nativeStyleHeight < height )
           height = nativeStyleHeight;
           
           
         if( width > parentWidth ) width = parentWidth;
-
-        
-          for(var i = 0; i < scroll_i.children.length; i++){
-            sc_i = scroll_i.children[i];  
-          }
-
           
 
           if( AScrollerBox === scroller_in ){
-            AScrollerBoxMax.style.width = window.innerWidth + "px";
-            AScrollerBoxMax.style.height = window.innerHeight + "px";
             width = window.innerWidth;
             height = window.innerHeight - ( (condition && condition.offsetHeight) || 0 ) - ( (scroller_in.condition && scroller_in.condition.offsetHeight) || 0 ) - (  scroller_in.mini ? scroller_in.mini.condition ? scroller_in.mini.offsetHeight + scroller_in.mini.condition.clientHeight : scroller_in.mini.offsetHeight : 0 );
           } else if( !nativeStyleHeight || gallery ){
@@ -331,17 +326,19 @@ function AScroller(scroller_in, curElement){
             height = scroller_area.offsetHeight;
           }
 
-          
-          if( gallery && width )scroll_i.children.cssScroller({'max-width' : width + "px" 
-          ,'width': width + "px" 
-          , "max-height" : height + "px" });
-
-          scroll_i.children.cssScroller({ 'max-width' : width + "px" , "max-height" : height + "px" , "height" : "inherit"  });
-
-          
-          if( gallery && width ) [scroller,scroller_area].cssScroller({'width': width + ( max_m_t + max_m_b ) + 'px' });
-            [scroller_in,scroller_area].cssScroller({'height': height + ( max_m_t + max_m_b ) + 'px' });
+          for(var i = 0; i < scroll_i.children.length; i++){
+            sc_i = scroll_i.children[i];  
             
+            if( gallery && width )
+              sc_i.cssScroller({'max-width' : width - (sc_i.dim.m_l + sc_i.dim.m_r) + "px" ,'width': width - (sc_i.dim.m_l + sc_i.dim.m_r) + "px" , "max-height" : height - (sc_i.dim.m_t + sc_i.dim.m_b) + "px"});
+            else
+              scroll_i.children.cssScroller({ 'max-width' : width - (sc_i.dim.m_l + sc_i.dim.m_r) + "px" , "max-height" : height - (sc_i.dim.m_t + sc_i.dim.m_b) + "px" , "height" : "inherit"  });
+
+          }
+
+          
+          if( gallery && width ) [scroller,scroller_area].cssScroller({'width': width + 'px' });
+            [scroller_in,scroller_area].cssScroller({'height': height  + 'px' });
           
 
         scroller.cssScroller({'position':'relative'});
